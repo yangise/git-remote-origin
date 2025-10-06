08df9449f2de29f1ad5b2f6b3a8bf6690d3479ba
양이세 <diddltp00@naver.com>
2025-10-06 18:56:24 +0900

    연습입니당

---
+++ Assets/NewEmptyCSharpScript.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class NewEmptyCSharpScript : MonoBehaviour
+{
+   void Start()
+   {
+      Debug.Log(message: "Hello World!");
+   }
+
+   private void Update()
+   {
+      
+      {
+         
+      }
+   }
+}
